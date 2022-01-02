f5b4bee16d08684bae8e3047f42771c1b7f832a6
asbelljc <asbelljc@gmail.com>
2022-01-02 00:06:33 -0500

    Made model files, finishing post.js.

---
+++ models/post.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+const { DateTime } = require('luxon');
+
+const PostSchema = new Schema({
+  title: { type: String, required: true },
+  date_time: { type: Date, default: Date.now() },
+  body: { type: String, required: true },
+  published: { type: Boolean, default: false },
+});
+
+PostSchema.virtual('date_time_formatted').get(function () {
+  const dtFormat = {
+    ...DateTime.DATE_MED_WITH_WEEKDAY,
+    ...DateTime.TIME_SIMPLE,
+  };
+  return this.date_time.toLocaleString(dtFormat);
+});
+
+module.exports = mongoose.model('Post', PostSchema);
