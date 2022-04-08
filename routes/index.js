b937c5bd347e69735b474343c0e9292dd029e379
asbelljc <asbelljc@gmail.com>
2022-04-08 17:49:15 -0400

    Renamed admin router to auth.

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const postsRouter = require('./posts');
-const adminRouter = require('./admin');
+const authRouter = require('./auth');
 
 // Only redirect GET requests to postsRouter; force use of /posts/* for requests that edit data
 router.get('/', function (req, res, next) {
@@ -10,6 +10,6 @@ router.get('/', function (req, res, next) {
 });
 
 router.use('/posts', postsRouter);
-router.use('/admin', adminRouter);
+router.use('/auth', authRouter);
 
 module.exports = router;
